876d55aeab7a2ccd8a8e7e347d7ca70e9b90be8f
Alan Ngai <alan@opsclarity.com>
2019-08-17 01:20:09 -0700

    integrated akka streams to flux

--- src/main/java/com/box/prototype/chatservice/rest/handler/ChatSessionHandler.java
+++ src/main/java/com/box/prototype/chatservice/rest/handler/ChatSessionHandler.java
@@ -1,15 +1,18 @@
 package com.box.prototype.chatservice.rest.handler;
 
+import akka.NotUsed;
 import akka.actor.AbstractActor;
 import akka.actor.AbstractActorWithTimers;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.stream.DelayOverflowStrategy;
 import akka.stream.javadsl.AsPublisher;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import com.box.prototype.chatservice.akka.AkkaComponents;
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -35,35 +38,35 @@ public class ChatSessionHandler implements WebSocketHandler {
 
     private ConcurrentHashMap<String, ActorRef> sessions = new ConcurrentHashMap<>();
 
-    // TODO: remove this temp actor that generates periodic messages for test
-    private static class FakeChatSession extends AbstractActorWithTimers {
-        private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
-
-        private static final Object TICK_KEY = "tick-key";
-        private static final class Tick {}
-        private static final class KillSession {}
-
-        /** constructor */
-        public FakeChatSession() {
-            getTimers().startPeriodicTimer(TICK_KEY, new Tick(), Duration.ofSeconds(2));
-        }
-
-        /** handle actor cleanup */
-        @Override
-        public void postStop() {
-            getTimers().cancel(TICK_KEY);
-        }
-
-        /** message handler */
-        @Override
-        public Receive createReceive() {
-            return receiveBuilder()
-                    .match(Tick.class, msg -> getSender().tell(String.format("%s: hello!", System.currentTimeMillis()), getSelf()))
-                    .match(KillSession.class, msg -> getContext().stop(getSelf()))
-                    .matchAny(msg -> log.info("received unknown message: ", msg))
-                    .build();
-        }
-    }
+//    // TODO: remove this temp actor that generates periodic messages for test
+//    private static class FakeChatSession extends AbstractActorWithTimers {
+//        private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+//
+//        private static final Object TICK_KEY = "tick-key";
+//        private static final class Tick {}
+//        private static final class KillSession {}
+//
+//        /** constructor */
+//        public FakeChatSession() {
+//            getTimers().startPeriodicTimer(TICK_KEY, new Tick(), Duration.ofSeconds(2));
+//        }
+//
+//        /** handle actor cleanup */
+//        @Override
+//        public void postStop() {
+//            getTimers().cancel(TICK_KEY);
+//        }
+//
+//        /** message handler */
+//        @Override
+//        public Receive createReceive() {
+//            return receiveBuilder()
+//                    .match(Tick.class, msg -> getSender().tell(String.format("%s: hello!", System.currentTimeMillis()), getSelf()))
+//                    .match(KillSession.class, msg -> getContext().stop(getSelf()))
+//                    .matchAny(msg -> log.info("received unknown message: ", msg))
+//                    .build();
+//        }
+//    }
 
     /** web socket handler */
     @Override
@@ -73,8 +76,8 @@ public class ChatSessionHandler implements WebSocketHandler {
             logger.info("starting websocket session [{}]", sessionId);
 
             // create chat session actor
-            ActorRef chatSession = this.akkaComponents.getSystem().actorOf(Props.create(FakeChatSession.class), "chat-session-" + sessionId);
-            this.sessions.put(sessionId, chatSession);
+//            ActorRef chatSession = this.akkaComponents.getSystem().actorOf(Props.create(FakeChatSession.class), "chat-session-" + sessionId);
+//            this.sessions.put(sessionId, chatSession);
 
 //            // echo inbound messages outbound
 //            final Flux<WebSocketMessage> outFlux = session.receive()
@@ -94,13 +97,28 @@ public class ChatSessionHandler implements WebSocketHandler {
 //                        ses.tell(new FakeChatSession.KillSession(), this.akkaComponents.getSystem().deadLetters());
 //                    }
 //                });
-            Source.queue()
-            final Flux<WebSocketMessage> outFlux =
-                    Flux.concat(Source
-                        .actorRef(Props.create(FakeChatSession.class))
-                        .runWith(Sink.asPublisher(AsPublisher.WITHOUT_FANOUT), this.akkaComponents.getMaterializer())
-                    )
-                    .map(msg -> session.textMessage(msg.toString()));
+
+
+            final Publisher<WebSocketMessage> outFlux = Source
+                .repeat(NotUsed.getInstance())
+                .delay(Duration.ofSeconds(1), DelayOverflowStrategy.backpressure())
+                .map(e -> "foo")
+                .map(session::textMessage)
+                .watchTermination((notUsed, done) -> {
+                    done.exceptionally(ex -> {
+                        logger.error("encountered error", ex);
+                        return null;
+                    });
+                    return NotUsed.getInstance();
+                })
+                .runWith(Sink.asPublisher(AsPublisher.WITHOUT_FANOUT), this.akkaComponents.getMaterializer());
+
+            // TODO: handle connection termination
+
+            // TODO: handle incoming message
+
+            // TODO: connect to chatroom
+            
             return session.send(outFlux);
         } else {
             logger.error("terminating due to duplicate session id: " + sessionId);
